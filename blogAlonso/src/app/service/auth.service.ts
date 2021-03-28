8d0d53d673dd6754155d883da0f4a741a686b430
AMS <alonso_mendess@mail.com>
2021-03-28 17:36:50 -0300

    Implementação do post e get de postagens

--- blogAlonso/src/app/service/auth.service.ts
+++ blogAlonso/src/app/service/auth.service.ts
@@ -22,13 +22,17 @@ export class AuthService {
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
   }
 
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`http://localhost:8080/usuarios/${id}`)
+  }
+
   logado() {
     let ok: boolean = false
 
     if(environment.token != ''){
       ok = true
     }
-    
+
     return ok
   }
 }
